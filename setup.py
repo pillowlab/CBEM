169f95bf7fb6c717def5ac09013845cc71186d07
latimerk <latimerkw@gmail.com>
2022-05-17 17:30:53 -0500

    adding environment

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(name="pyCBEM",
+    version="0.1",
+    url="https://github.com/pillowlab/CBEM",
+    author="Kenneth W. Latimer",
+    author_email="latimerkw@gmail.com",
+    license="MIT",
+    install_requires=["numpy", "jax", "scipy"],
+    packages=["pyCBEM"]
+      )
+
+      
